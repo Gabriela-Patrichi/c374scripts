ec182870f0c31f0acd38ebb8e5b91a8875b6df31
EC2 Default User <ec2-user@ip-172-31-47-15.eu-north-1.compute.internal>
2024-02-04 19:49:15 +0000

    activity 3 completed

--- Activity3/script2.sh
+++ Activity3/script2.sh
@@ -10,7 +10,8 @@
 
 echo "Please enter 5 numbers:"
 
-for ((i=1; i<=5; i++)); do
+for ((i=1; i<=5; i++))
+do
     read -p "Number $i: " user_input
     echo $user_input >> results_$(date +'%d%m%Y')
 done
@@ -19,3 +20,9 @@ echo "Please enter the bonus number:"
 
 read user_input
     echo $user_input >> results_$(date +'%d%m%Y')
+
+    matching_count=$(grep -w -x -f "results_$(date +'%d%m%Y')" "numbers_$(date +'%d%m%Y')" | wc -l)
+
+echo "Matching numbers: $matching_count"
+
+rm "results_$(date +'%d%m%Y')"
