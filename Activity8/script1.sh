b971026b8e8dd1b4bc9a458a6883a292058befed
EC2 Default User <ec2-user@ip-172-31-47-15.eu-north-1.compute.internal>
2024-02-05 12:18:43 +0000

    activity 8 completed

--- Activity8/script1.sh
+++ Activity8/script1.sh
@@ -42,3 +42,24 @@ do
 done
 
 echo "Numbers entered: " ${array[@]}
+
+product=1
+sum=0
+min=${array[0]}
+max=${array[0]}
+
+for i in ${array[@]}
+do
+	product=$((product * i))
+	sum=$((sum + i))
+	min=$((i < min ? i : min))
+	max=$((i > max ? i : max))
+done
+
+average=$(bc -l <<< "scale=2; $sum / ${#array[@]}")
+
+echo "Product of the integers: $product"
+echo "Average of the integers: $average"
+echo "Sum of the integers: $sum"
+echo "Min of the  integers: $min"
+echo "Max of the integers: $max"
